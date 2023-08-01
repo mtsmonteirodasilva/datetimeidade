5f47074887ff7510eac1be6efa60ea8d2669053c
Matheus Monteiro <matheus2016monteiro@gmail.com>
2023-08-01 14:07:25 -0400

    Primeiro commit

---
+++ datetimeidade/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace datetimeidade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtp_nascimento = new System.Windows.Forms.DateTimePicker();
+            this.bt_calcular = new System.Windows.Forms.Button();
+            this.lb_resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtp_nascimento
+            // 
+            this.dtp_nascimento.Location = new System.Drawing.Point(12, 12);
+            this.dtp_nascimento.Name = "dtp_nascimento";
+            this.dtp_nascimento.Size = new System.Drawing.Size(200, 20);
+            this.dtp_nascimento.TabIndex = 0;
+            // 
+            // bt_calcular
+            // 
+            this.bt_calcular.Location = new System.Drawing.Point(137, 183);
+            this.bt_calcular.Name = "bt_calcular";
+            this.bt_calcular.Size = new System.Drawing.Size(75, 23);
+            this.bt_calcular.TabIndex = 1;
+            this.bt_calcular.Text = "Calcular";
+            this.bt_calcular.UseVisualStyleBackColor = true;
+            this.bt_calcular.Click += new System.EventHandler(this.bt_calcular_Click);
+            // 
+            // lb_resultado
+            // 
+            this.lb_resultado.AutoSize = true;
+            this.lb_resultado.Location = new System.Drawing.Point(23, 222);
+            this.lb_resultado.Name = "lb_resultado";
+            this.lb_resultado.Size = new System.Drawing.Size(35, 13);
+            this.lb_resultado.TabIndex = 2;
+            this.lb_resultado.Text = "label1";
+            this.lb_resultado.Click += new System.EventHandler(this.lb_resultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 268);
+            this.Controls.Add(this.lb_resultado);
+            this.Controls.Add(this.bt_calcular);
+            this.Controls.Add(this.dtp_nascimento);
+            this.Name = "Form1";
+            this.Text = "Calcular idade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtp_nascimento;
+        private System.Windows.Forms.Button bt_calcular;
+        private System.Windows.Forms.Label lb_resultado;
+    }
+}
+
---
+++ datetimeidade/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datetimeidade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lb_resultado.Text = "";
+        }
+
+        private void bt_calcular_Click(object sender, EventArgs e)
+        {
+            DateTime dataN = dtp_nascimento.Value;
+            DateTime dataHj = new DateTime(2023, 08, 01);
+            TimeSpan dias = dataHj - dataN;
+            double idade = dias.Days/365.2425;
+            double idade2 = (int)Math.Truncate(idade);
+           
+            lb_resultado.Text = ($"Sua idade é de: {idade2.ToString()} anos");
+            
+        }
+
+        private void lb_resultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
